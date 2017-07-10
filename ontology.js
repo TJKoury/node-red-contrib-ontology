922329084895cafaf433095494b4b55cf646f5e2
TJKoury <tjkoury@gmail.com>
2017-07-10 09:04:14 -0400

    i am so smrt

--- ontology.js
+++ ontology.js
@@ -14,35 +14,78 @@
  * limitations under the License.
  **/
 
-module.exports = function(RED) {
+
+
+module.exports = function (RED) {
     "use strict";
 
+    var path = require('path');
+    var fs = require('fs');
     function ontologyNode(n) {
-  
-        RED.nodes.createNode(this,n);
+
+        RED.nodes.createNode(this, n);
+        
+        this.knex = (RED.hasOwnProperty("cluster") && RED.cluster.knex) ? RED.cluster.knex : require('knex');
 
         this.topic = n.topic;
 
         var node = this;
+        for (var x in n) {
+            node[x] = n[x];
+        }
+       process.env.knexStart = process.env.knexStart || "";
+        if (process.bingo === process.pid && process.env.knexStart.indexOf(node.name)===-1) {
+           process.env.knexStart+=node.name+",";
 
-        var msg = {};
-        msg.topic = this.topic;
-        msg.payload = "Hello world !"
+            var ontology_path = path.resolve(process.env.NODE_RED_HOME, 'databases', 'ontology');
 
-        this.send(msg);
+            if (!fs.existsSync(ontology_path)) {
+                fs.mkdirSync(ontology_path);
+            }
+
+            var file_name = path.resolve(ontology_path, this.name + ".us-ontology");
+
+            this.knex_client = this.knex({
+                client: 'sqlite3',
+                useNullAsDefault: true,
+                connection: {
+                    filename: file_name
+                }
+            });
+
+            Promise.all([this.knex_client.schema.raw('SELECT * FROM sqlite_master'),
+            this.knex_client.schema.dropTableIfExists(node.name).catch(function(e){
+                console.log(e)
+            }),
+            this.knex_client.schema.createTable(node.name, function (table) {
+
+                table.increments();
+                table.string('name');
+                table.timestamps();
+            }).catch(function(e){
+                console.log(e);
+            })]).then((dbSchema, dropTableResult, createTableResult)=>{
+                
+                console.log(process.pid, process.bingo,process.env.knexStart, new Date())
+            })
+
+        }
+
 
         // respond to inputs....
         this.on('input', function (msg) {
-            node.warn("I saw a payload: "+msg.payload);
-         
+            node.warn("I saw a payload: " + msg.payload);
+
             node.send(msg);
         });
 
-        this.on("close", function() {
-      
+        this.on("close", function () {
+            if(process.knexStart &&process.env.knexStart[node.name]){
+                deleteprocess.env.knexStart[node.name];
+            }
         });
     }
 
-    RED.nodes.registerType("ontology",ontologyNode);
+    RED.nodes.registerType("ontology", ontologyNode);
 
 }
